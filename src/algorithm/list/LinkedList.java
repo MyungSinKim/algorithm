7e8383cd05e9d8b8204da6929735ce12ec0afc35
morethink <lwhyx9@qq.com>
2017-08-20 18:57:27 +0800

    链表面试题,合并

--- src/algorithm/list/LinkedList.java
+++ src/algorithm/list/LinkedList.java
@@ -12,12 +12,15 @@ import java.util.Stack;
  * 3. 查找单链表中的倒数第K个结点（k > 0）
  * 4. 查找单链表的中间结点
  * 5. 从尾到头打印单链表
- * 6. 已知两个单链表pHead1 和pHead2 各自有序，把它们合并成一个链表依然有序
+ * 6. 已知两个单链表pHead1和pHead2各自有序，把它们合并成一个链表依然有序
  * 7. 判断一个单链表中是否有环
- * 8. 判断两个单链表是否相交
- * 9. 求两个单链表相交的第一个节点
- * 10. 已知一个单链表中存在环，求进入环中的第一个节点
- * 11. 给出一单链表头指针pHead和一节点指针pToBeDeleted，O(1)时间复杂度删除节点pToBeDeleted
+ * 8. 已知一个单链表中存在环，求进入环中的第一个节点
+ * 9. 判断两个单链表是否相交
+ * 10. 求两个单链表相交的第一个节点
+ * 11. 给出一单链表头指针 head 和一节点指针 deletedNode，O(1)时间复杂度删除节点deletedNode
+ * 12. 链表的冒泡排序
+ * 13. 单链表的双冒泡排序
+ * 快慢指针，新的链表
  */
 
 
@@ -30,24 +33,19 @@ public class LinkedList {
     public static void main(String[] args) {
         Node head = null, head2 = null;
         for (int i = 1; i <= 5; i++) {
-//            head = addBack(head, (int) (Math.random() * 100));
             head = addBack(head, i);
         }
         for (int i = 2; i <= 6; i++) {
-//            head = addBack(head, (int) (Math.random() * 100));
             head2 = addBack(head2, i);
         }
         show(head);
-//        sort(head, null);
-//        show(head);
-
-
         System.out.println("1. 求单链表中结点的个数");
         System.out.println("size=" + size(head));
 
         System.out.println("2. 将单链表反转");
-//        head = (reverse(head));
-//        show(head);
+        head = reverse(head);
+        show(head);
+        head = reverse(head);
 
         System.out.println("3. 查找单链表中的倒数第K个结点（k > 0）");
         Node kNode = getKNode(head, 6);
@@ -62,15 +60,73 @@ public class LinkedList {
         System.out.println();
 
         System.out.println("6. 已知两个单链表pHead1 和pHead2 各自有序，把它们合并成一个链表依然有序");
-        Node node = merge(head, head2);
+        Node head6 = null, head7 = null;
+        for (int i = 1; i <= 10; i++) {
+            head6 = addBack(head6, (int) (Math.random() * 100));
+        }
+        bubbleSort(head6);
+
+        for (int i = 1; i <= 10; i++) {
+            head7 = addBack(head7, (int) (Math.random() * 100));
+        }
+        bubbleSort(head7);
+        merge(head6, head7);
 
         System.out.println("7. 判断一个单链表中是否有环");
-        System.out.println("8. 判断两个单链表是否相交");
-        System.out.println("9. 求两个单链表相交的第一个节点");
-        System.out.println("10. 已知一个单链表中存在环，求进入环中的第一个节点");
-        System.out.println("11. 给出一单链表头指针pHead和一节点指针pToBeDeleted，O(1)时间复杂度删除节点pToBeDeleted");
+        Node head3 = null;
+        for (int i = 1; i <= 5; i++) {
+            head3 = addBack(head3, i);
+        }
+        Node p = head3;
+        while (p.next != null) {
+            p = p.next;
+        }
 
-        System.out.println();
+        p.next = head3.next;
+
+        System.out.println(hasRing(head3));
+
+        System.out.println("8. 已知一个单链表中存在环，求进入环中的第一个节点");
+        System.out.println(getFirstRingNode(head3).num);
+        System.out.println("9. 判断两个单链表是否相交");
+        Node head4 = null, head5 = null, p4;
+        for (int i = 2; i <= 6; i++) {
+            head4 = addBack(head4, i);
+        }
+        p4 = head4;
+        while (p4.next != null) {
+            p4 = p4.next;
+        }
+        p4.next = head.next;
+        for (int i = 2; i <= 6; i++) {
+            head5 = addBack(head5, i);
+        }
+        p4 = head5;
+        while (p4.next != null) {
+            p4 = p4.next;
+        }
+        p4.next = head.next;
+        System.out.println(isIntersect(head4, head5));
+
+        System.out.println("10. 求两个单链表相交的第一个节点");
+        System.out.println(firstIntersectNode(head4, head5).num);
+        System.out.println("11. 给出一单链表头指针 head 和一节点指针 deletedNode，O(1)时间复杂度删除节点deletedNode");
+
+        head = delete(head, head);
+        show(head);
+        System.out.println("12. 单链表的冒泡排序");
+        Node head12 = null;
+        for (int i = 1; i <= 10; i++) {
+            head12 = addBack(head12, (int) (Math.random() * 100));
+        }
+        bubbleSort(head12);
+        System.out.println("13. 单链表的双冒泡排序");
+        Node head13 = null;
+        for (int i = 1; i <= 10; i++) {
+            head13 = addBack(head13, (int) (Math.random() * 100));
+        }
+        doubleBubblesort(head13, null);
+        show(head13);
     }
 
     public static Node addBack(Node head, int num) {
@@ -181,7 +237,7 @@ public class LinkedList {
         }
         Node p1 = head, p2 = head;
         while (p1.next != null) {
-            if (p1.next.next == null) {
+            if (p1 == null) {
                 break;
             }
             p1 = p1.next.next;
@@ -224,39 +280,188 @@ public class LinkedList {
 
     /**
      * 6. 已知两个单链表pHead1 和pHead2 各自有序，把它们合并成一个链表依然有序
+     * 类似于归并排序
      *
      * @param head1
      * @param head2
      * @return
      */
     public static Node merge(Node head1, Node head2) {
-        Node head = null;
-        Node p1 = null, p2 = null;
-        if (head1.num > head2.num) {
-            head = head2;
-            p2 = head1;
-        } else {
+        Node p1 = head1, p2 = head2, head;
+        if (head1.num < head2.num) {
             head = head1;
-            p2 = head2;
+            p1 = p1.next;
+        } else {
+            head = head2;
+            p2 = p2.next;
         }
-        p1 = head.next;
-        while (p1 != null || p2 != null) {
-            if (p1.num > p2.num) {
-                Node temp = p1;
+
+        Node p = head;
+        while (p1 != null && p2 != null) {
+            if (p1.num <= p2.num) {
+                p.next = p1;
                 p1 = p1.next;
-                temp.next = p2;
+                p = p.next;
+            } else {
+                p.next = p2;
+                p2 = p2.next;
+                p = p.next;
             }
+        }
+        if (p1 != null) {
+            p.next = p1;
+        }
+        if (p2 != null) {
+            p.next = p2;
         }
         return head;
     }
 
     /**
-     * 单链表的双冒泡排序
+     * 7. 判断一个单链表中是否有环
+     * <p>
+     * 这里也是用到两个指针。如果一个链表中有环，也就是说用一个指针去遍历，
+     * 是永远走不到头的。因此，我们可以用两个指针去遍历，一个指针一次走两步，
+     * 一个指针一次走一步，如果有环，两个指针肯定会在环中相遇。时间复杂度为O（n）
+     *
+     * @param head
+     * @return
+     */
+    public static boolean hasRing(Node head) {
+        Node p1 = head, p2 = head;
+        while (p1 != null && p1.next != null) {
+            p1 = p1.next.next;
+            p2 = p2.next;
+            if (p1 == p2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 8. 已知一个单链表中存在环，求进入环中的第一个节点
+     *
+     * @param head
+     * @return
+     */
+    public static Node getFirstRingNode(Node head) {
+        Node p1 = head, p2 = head;
+        while (p1 != null && p1.next != null) {
+            p1 = p1.next.next;
+            p2 = p2.next;
+            if (p1 == p2) {
+                p1 = head;
+                while (p1 != p2) {
+                    p1 = p1.next;
+                    p2 = p2.next;
+                }
+                break;
+            }
+        }
+        return p1;
+    }
+
+    /**
+     * 9. 判断两个单链表是否相交
+     * 如果两个链表相交，那么相交之后的节点应该相同，那么最后那个节点应该也相同
+     *
+     * @param head1
+     * @param head2
+     * @return
+     */
+    public static boolean isIntersect(Node head1, Node head2) {
+        Node p1 = head1, p2 = head2;
+        while (p1.next != null) {
+            p1 = p1.next;
+        }
+        while (p2.next != null) {
+            p2 = p2.next;
+        }
+        return p1 == p2;
+    }
+
+    /**
+     * 10. 求两个单链表相交的第一个节点
+     * 采用对齐的思想。计算两个链表的长度 L1 , L2，分别用两个指针 p1 , p2 指向两个链表的头，
+     * 然后将较长链表的 p1（假设为 p1）向后移动L2 - L1个节点，然后再同时向后移动p1 , p2，
+     * 直到 p1 = p2。相遇的点就是相交的第一个节点。
+     *
+     * @param head1
+     * @param head2
+     * @return
+     */
+    public static Node firstIntersectNode(Node head1, Node head2) {
+        int len1 = size(head1);
+        int len2 = size(head2);
+        Node p1 = head1, p2 = head2;
+        if (len1 > len2) {
+            for (int i = 1; i < len1 - len2; i++) {
+                p1 = p1.next;
+            }
+        } else {
+            for (int i = 1; i < len2 - len1; i++) {
+                p2 = p2.next;
+            }
+        }
+        while (p1 != p2) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return p1;
+    }
+
+    /**
+     * 11. 给出一单链表头指针 head 和一节点指针 deletedNode，O(1)时间复杂度删除节点deletedNode
+     *
+     * @param head
+     */
+    public static Node delete(Node head, Node deletedNode) {
+        if (deletedNode.next != null) {
+            deletedNode.num = deletedNode.next.num;
+            deletedNode.next = deletedNode.next.next;
+        } else {
+            if (head == deletedNode) {
+                return null;
+            }
+            Node p = head;
+            while (p.next != deletedNode) {
+                p = p.next;
+            }
+            p.next = null;
+        }
+        return head;
+    }
+
+    /**
+     * 12 单链表的冒泡排序
+     *
+     * @param head
+     */
+    public static void bubbleSort(Node head) {
+        Node tail = null;
+        Node p1;
+        while (head != tail) {
+            for (p1 = head; p1.next != tail; p1 = p1.next) {
+                if (p1.num > p1.next.num) {
+                    int temp = p1.num;
+                    p1.num = p1.next.num;
+                    p1.next.num = temp;
+                }
+            }
+            tail = p1;
+        }
+        show(head);
+    }
+
+    /**
+     * 13 单链表的双冒泡排序
      *
      * @param start
      * @param end
      */
-    public static void sort(Node start, Node end) {
+    public static void doubleBubblesort(Node start, Node end) {
         if (start != end) {
             Node p1 = start;
             Node p2 = p1.next;
@@ -272,8 +477,8 @@ public class LinkedList {
             int temp = p1.num;
             p1.num = start.num;
             start.num = temp;
-            sort(start, p1);
-            sort(p1.next, null);
+            doubleBubblesort(start, p1);
+            doubleBubblesort(p1.next, null);
         }
 
     }
